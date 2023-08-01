5f45a2c898742e7146b62ad5ff397acac7f49243
alizarchcc <alizarchoudhary.cc@gmail.com>
2023-08-02 00:16:35 +0500

    [1] Create Token contract. [2] Create transfer function

---
+++ smart_contract.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.21;
+
+contract Token{
+    string public name;
+    string public symbol;
+    uint8 public decimal;
+    uint256 public totalSupply;
+
+    mapping (address => uint256) public balanceOf;
+    mapping (address => mapping (address => uint256)) public allowance;
+
+    event Tranfer(address indexed  from, address indexed to, uint256 value);
+    event Approval(address indexed  owner, address indexed spender, uint256 value);
+
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint8 _decimal,
+        uint256 _totalSupply
+    ){
+        name = _name;
+        symbol = _symbol;
+        decimal = _decimal;
+        totalSupply = _totalSupply;
+        balanceOf[msg.sender] = _totalSupply;
+    }
+
+    function transfer(address to, uint256 value) public returns (bool success){
+        require(to != address(0), "Invalid Address");
+        require(balanceOf[msg.sender] >= value, "Insufficient balance");
+
+        balanceOf[msg.sender] -= value;
+        balanceOf[to] += value;
+
+        emit Tranfer(msg.sender, to, value);
+        return true;
+    }
+}
